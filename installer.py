071b9b04b8f546f50b1794ceef364dbaf71252ac
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-06-07 23:38:48 +1200

    First iteration

---
+++ installer.py
@@ -0,0 +1,47 @@
+import os
+import time
+import platform
+import sys
+
+operating_system = platform.system()
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+RESET = "\033[0m"
+
+def install_jdk(operating_system):
+    
+    if operating_system == "Linux":
+        jdk = "default-jdk"
+
+        print(YELLOW + "[JAVA]\tPlease intall Java" + RESET)
+        os.system(f"sudo apt install {jdk}")
+    
+    elif operating_system == "Windows":
+        jdk = ".\\dependencies\\jdk-20_windows-x64_bin.exe"
+        jre = ".\\dependencies\\jre-8u371-windows-x64.exe"
+
+        print(YELLOW + "[JDK]\tPlease intall the Java JDK version" + RESET)
+        os.system(jdk)
+        print(YELLOW + "[JRE]\tPlease intall the Java JRE version" + RESET)
+        os.system(jre)
+        
+    else:
+        print(RED + "[!]\tUnsupported operating system." + RESET)
+        sys.exit(1)
+
+def eula_auto_accept():
+    with open(".\\server\\eula.txt", "w") as eula:
+        eula.write("eula=true")
+    
+
+time.sleep(2)
+
+print("\n"*5)
+install_jdk(operating_system)
+print(GREEN + "[i]\tAccepting the EULA for the minecraft server."+ RESET)
+eula_auto_accept()
+print(GREEN + "[✓]\tEverything should be installed and ready to go!"+ RESET)
+time.sleep(5)
+sys.exit(0)
---
+++ run.bat
@@ -0,0 +1,1 @@
+java -Xmx4069M -Xms1024M -jar server.jar nogui
